ee4de98ec784ebb89f792cf9be15ecbe6cbe7296
Maile.Rohiväli <maile.rohivali@gmail.com>
2014-09-09 13:05:31 +0300

    Add sample variables

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>harjutus-3 - PHP andmetüübid</title>
+</head>
+  <body>
+
+
+
+    <h1>Muutujad</h1>
+    <?php 
+     $variable = 30;
+     echo $variable;
+     //Lisan veebilehitsejas parema loetavuse saamiseks reamurdmise elemendi:
+     echo "<br>";
+    ?>
+
+       
+  </body>
+</html>
